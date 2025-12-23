d5d6da98aa0793fa7e8f80b5644931b61731ce7a
William Quiceno <wquiceno@gmail.com>
2025-12-23 07:44:50 -0500

    Add Firebase web SDK and token button

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-// TODO: pega aquí tu firebaseConfig real
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "TODO",
-    authDomain: "TODO",
-    projectId: "TODO",
-    storageBucket: "TODO",
-    messagingSenderId: "TODO",
-    appId: "TODO"
+    apiKey: "AIzaSyD9ol1makDaZ2PuUSzCKz2zbT66HaYoT5o",
+    authDomain: "horarios-pwa.firebaseapp.com",
+    projectId: "horarios-pwa",
+    storageBucket: "horarios-pwa.firebasestorage.app",
+    messagingSenderId: "460075202224",
+    appId: "1:460075202224:web:f14d769b208ba02986ad75"
 };
